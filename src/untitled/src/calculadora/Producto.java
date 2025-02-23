9ffdf6148cb04a8bb41e1be04f7a98c01aecf90c
sabrinaunir <sabrina.aigbogun690@comunidadunir.net>
2025-02-23 17:06:17 +0100

    Update Producto.java
    añadir comentarios

--- src/untitled/src/calculadora/Producto.java
+++ src/untitled/src/calculadora/Producto.java
@@ -2,18 +2,62 @@ package calculadora;
 
 public class Producto {
 
+    /**
+     * Multiplica dos números reales.
+     */
     public double multiplicar(double a, double b) {
         return a * b;
     }
 
+    /**
+     * Multiplica dos números enteros.
+     */
     public int multiplicar(int a, int b) {
         return a * b;
     }
 
+    /**
+     * Multiplica tres números reales.
+     */
     public double multiplicar(double a, double b, double c) {
         return a * b * c;
     }
 
+    /**
+     * Calcula la potencia de un número.
+     */
+    public double potencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
+}
+package calculadora;
+
+public class Producto {
+
+    /**
+     * Multiplica dos números reales.
+     */
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    /**
+     * Multiplica dos números enteros.
+     */
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Multiplica tres números reales.
+     */
+    public double multiplicar(double a, double b, double c) {
+        return a * b * c;
+    }
+
+    /**
+     * Calcula la potencia de un número.
+     */
     public double potencia(double base, double exponente) {
         return Math.pow(base, exponente);
     }
