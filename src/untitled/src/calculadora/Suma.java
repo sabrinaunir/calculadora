dd62f92ab70d23163427fd02653010d68d6590cb
sabrinaunir <sabrina.aigbogun690@comunidadunir.net>
2025-02-23 17:05:31 +0100

    Update Suma.java
    añadir comentarios

--- src/untitled/src/calculadora/Suma.java
+++ src/untitled/src/calculadora/Suma.java
@@ -4,22 +4,37 @@ public class Suma {
 
     private double acumulado;
 
+    /**
+     * Constructor de la clase Suma.
+     */
     public Suma() {
         this.acumulado = 0;
     }
 
+    /**
+     * Suma dos números reales.
+     */
     public double sumar(double a, double b) {
         return a + b;
     }
 
+    /**
+     * Suma dos números enteros.
+     */
     public int sumar(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Suma tres números reales.
+     */
     public double sumar(double a, double b, double c) {
         return a + b + c;
     }
 
+    /**
+     * Suma un número al valor acumulado.
+     */
     public double sumarAcumulado(double a) {
         this.acumulado += a;
         return this.acumulado;
