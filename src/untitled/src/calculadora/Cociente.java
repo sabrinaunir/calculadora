05ca7fb77a67f7d04bed18001c924f2bce3e2c4e
sabrinaunir <sabrina.aigbogun690@comunidadunir.net>
2025-02-23 17:06:58 +0100

    Update Cociente.java
    añadir comentarios

--- src/untitled/src/calculadora/Cociente.java
+++ src/untitled/src/calculadora/Cociente.java
@@ -2,16 +2,24 @@ package calculadora;
 
 public class Cociente {
 
+    /**
+     * Divide dos números reales.
+     */
     public double dividir(double a, double b) {
         return a / b;
     }
 
+    /**
+     * Divide dos números enteros.
+     */
     public int dividir(int a, int b) {
         return a / b;
     }
 
+    /**
+     * Calcula la raíz cuadrada de un número.
+     */
     public double raiz(double a) {
         return Math.sqrt(a);
-
     }
 }
